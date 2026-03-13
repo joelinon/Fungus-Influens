9a9f913da7e8524e030585b434e1056f5a1cfd2d
joelinon <joelun23@utb.karlskoga.se>
2026-03-13 11:52:42 +0100

    edwef

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "30%";
+  document.getElementById("spanien").classList.add("noshow");
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("spanien").classList.remove("noshow");
+}
